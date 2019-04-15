72442aeb0b84136cf903c044cfe1ecc7790ec526
Ramana <pvrr1310@gmail.com>
2019-04-15 15:17:41 +0530

    new project added

---
+++ com.org.sample2/src/main/java/com/org/sample2/com/org/sample2/App.java
@@ -0,0 +1,13 @@
+package com.org.sample2.com.org.sample2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
